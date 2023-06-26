0b99e9659c47be0193c0107319b853d28ad2b1ed
LiuTianyou <1347277058@qq.com>
2023-06-26 21:03:27 +0800

    读入数据函数更新

--- src/main.asm
+++ src/main.asm
@@ -4,9 +4,10 @@
 main:
     ; 加载 assets/menu.bmp
     ; 0x100000 - 0x1C0400
-    mov eax, 2 ; 起始扇区
-    mov ebx, 0x100000
-    %include 'tools/draw/readBitmap.asm'
+    push dword 1538 ; 扇区数量 1538
+    push dword 0x100000 ; 写入地址
+    push dword 2 ; 起始扇区
+    %include 'tools/disk/readData.asm'
     push dword 0x00 ; 起始位置
     push word 768 ; 列数
     push word 1024 ; 行数
