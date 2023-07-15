743c1f8e6346632e8e6bef33cdc6e258d6b4b749
L1utongwei <1347277058@qq.com>
2023-07-15 21:26:24 +0800

    16x16 font

--- src/simple_print.h
+++ src/simple_print.h
@@ -1,8 +1,8 @@
 #pragma once
 #include "ASCII_font.h"
 int pos_x = 0, pos_y = 0; //text position
-inline byte getBit(byte number, byte offset){
-    return (number >> (6 - offset)) & 1; 
+inline uint16_t getBit(uint16_t number, uint16_t offset){
+    return (number >> (16 - offset)) & 1; 
 }
 void clear_screen(color* buffer, color bc){
     int i, j;
@@ -15,24 +15,22 @@ void clear_screen(color* buffer, color bc){
 void putchar(color* buffer, unsigned char ch, color fc, color bc){
     int i, j;
     if(ch == '\n' || ch == '\r'){
-        pos_x = 0;
-        pos_y += 2;
-        if(pos_y > 768 / 6) clear_screen(buffer, bc), pos_x = pos_y = 0;
+        pos_x = 0, pos_y += 1;
+        if(pos_y >= 768 / 16) clear_screen(buffer, bc), pos_x = pos_y = 0;
         return;
     }
     if(ch >= 'a' && ch <= 'z'){
-        ch -= 'a';
-        ch += 'A';
+        ch -= 'a', ch += 'A';
     }
-    byte* font = get_ASCII_font(ch);
-    for(i = pos_y * 6 + 1; i < (pos_y + 1) * 6 + 1; i++){
-        for(j = pos_x * 6 + 2; j < (pos_x + 1) * 6 + 2; j++){
-            buffer[i * 1024 + j] = getBit(font[i - pos_y * 6 - 1], j - pos_x * 6 - 1) ? fc : bc;
+    uint16_t* font = get_ASCII_font(ch);
+    for(i = pos_y * 16 + 1; i < (pos_y + 1) * 16 + 1; i++){
+        for(j = pos_x * 16 + 2; j < (pos_x + 1) * 16 + 2; j++){
+            buffer[i * 1024 + j] = getBit(font[i - pos_y * 16 - 1], j - pos_x * 16 - 1) ? fc : bc;
         }
     }
     pos_x++;
-    if(pos_x > 1024 / 6) pos_x = 0, pos_y++;
-    if(pos_y > 768 / 6) clear_screen(buffer, bc), pos_x = pos_y = 0;
+    if(pos_x >= 1024 / 16) pos_x = 0, pos_y++;
+    if(pos_y >= 768 / 16) clear_screen(buffer, bc), pos_x = pos_y = 0;
 }
 void print(color* buffer, unsigned char* string, color fc, color bc){
     while(*string != '\0'){
