42e0bea3355881f8237234d3d2a8e61a8687ef9d
L1utongwei <1347277058@qq.com>
2023-07-16 21:17:16 +0800

    disk read

--- src/main.c
+++ src/main.c
@@ -2,7 +2,7 @@
 void entry(unsigned char framebuffer[768][1024]){
     clear_screen(framebuffer, blue);
     byte* disk_buffer = malloc(512);
-    readSectors(disk_buffer, 40, 1);
+    readSectors(disk_buffer, 0, 1);
     for(int i = 0; i < 512; i++){
         putchar(framebuffer, disk_buffer[i], white, blue);
     }
